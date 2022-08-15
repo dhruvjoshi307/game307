5c7113f1bc49e60db8349182493de14d1e3fe263
dhruvjoshi307 <111216207+dhruvjoshi307@users.noreply.github.com>
2022-08-15 13:35:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("Please make sure you are playing this game in mobile beacause the some of the characters are not supported in Pc");
+alert("Please make sure you are playing this game in mobile and turn your internet connection ON");
